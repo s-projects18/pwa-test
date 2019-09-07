fbeb38f5a330ca615857225657c074450aef64ae
s-projects18 <44542045+s-projects18@users.noreply.github.com>
2019-09-07 09:56:59 +0200

    cache content

--- sw.js
+++ sw.js
@@ -1,10 +1,30 @@
+var cacheName = 'pwa-test-v1';
+var contentToCache = [
+    '/index.html',
+    '/icons/pwa-icon-192.png',
+    '/icons/pwa-icon-512.png',
+    '/icons/red.png',
+    '/icons/green.png'
+];
+
+// make content available offline by adding it to cache
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all');
+            return cache.addAll(contentToCache);
+        }).catch((err) => {
+            console.log(err);
+        })
+    );
+});
+
+// whenever an request if fired by the app
 self.addEventListener('fetch', function(e){
+    console.log('[Service Worker] Fetched resource '+e.request.url);
 });
 
-
-self.addEventListener('install', function(event) {
-});
-
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function(e) {
 
 });
